eadec55c3ca75e352457a7aacb4197ded49b7e1a
marzouq <marzouqaadebayo@gmail.com>
2022-12-09 23:16:46 +0000

    Task 2 - Prnit alphabets using ASCII codes

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * main - Entry Point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int count = 97;
+
+	while (count < 97 + 26)
+	{
+		putchar(count);
+		count++;
+	}
+	return (0);
+}
