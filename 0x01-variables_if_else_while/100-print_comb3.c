4a0b16716d96b065fdf63f2798348c09e807d014
marzouq <marzouqaadebayo@gmail.com>
2022-12-10 04:15:03 +0000

    Adv task 100

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+/**
+ * main - Entry Point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int n1 = 0, n2 = 0;
+
+	while (n1 < 10)
+	{
+		while (n2 < 10)
+		{
+			if (n2 < n1)
+			{
+				n2++;
+				continue;
+			}
+			putchar(48 + n1);
+			putchar(48 + n2);
+			if (n1 != 9 || n2 != 9)
+			{
+				putchar(44);
+				putchar(32);
+			}
+			n2++;
+		}
+		n1++;
+		n2 = 0;
+	}
+	return (0);
+}
