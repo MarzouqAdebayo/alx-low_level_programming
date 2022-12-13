d103752737c4ee43ee6fe85f7d76da495935a099
marzouq <marzouqaadebayo@gmail.com>
2022-12-13 20:07:45 +0000

    Task -11

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,12 @@
+#include "main.h"
+/**
+ * main - check the code
+ * @a: Parameter a
+ * @b: Parameter b
+ *
+ * Return: The addition of the two integers.
+ */
+int add(int a, int b)
+{
+	return (a + b);
+}
