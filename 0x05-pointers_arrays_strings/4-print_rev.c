ba4a7bc07f0e66e18f6b0bf966e5d447f7fd465d
marzouq <marzouqaadebayo@gmail.com>
2022-12-19 18:12:06 +0000

    Task-4

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,26 @@
+#include "main.h"
+/**
+ * _print_rev - Body of the function
+ * @str: pointer to string
+ *
+ * Description: Prints a string in reverse to stdout
+ * Return: has no return value
+ */
+void print_rev(char *str)
+{
+	int counter;
+
+	while (*str != '\0')
+	{
+		str++;
+		counter++;
+	}
+
+	while (counter > 0)
+	{
+		str--;
+		_putchar(*str);
+		counter--
+	}
+	_putchar('\n');
+}
