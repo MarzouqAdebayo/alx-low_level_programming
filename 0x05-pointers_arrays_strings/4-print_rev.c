22ffe0e883700abcf39190c2aac060c453ac5bfa
marzouq <marzouqaadebayo@gmail.com>
2022-12-20 05:09:05 +0000

    Tasks done

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -1,18 +1,18 @@
 #include "main.h"
 /**
  * print_rev - Body of the function
- * @str: pointer to string
+ * @s: pointer to string
  *
  * Description: Prints a string in reverse to stdout
  * Return: has no return value
  */
-void print_rev(char *str)
+void print_rev(char *s)
 {
 	int i;
 	char ch;
 
 	i = 0;
-	while (str[i] != '\0')
+	while (s[i] != '\0')
 	{
 		i++;
 	}
