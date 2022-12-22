bec8210627cc2cc67670d979e2c6b3bd0f93342b
marzouq <marzouqaadebayo@gmail.com>
2022-12-22 01:22:11 +0000

    Task 3 - strcmp

---
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ * *_strncpy - concatenates the copy of n bytes of the string pointed to
+ * by @src to the end of the string pointed to by @dest
+ * @dest: String that will be appended to
+ * @src: String to be concatenated upon
+ *
+ * Return: returns poiner to @dest
+ */
+
+char *_strncpy(char *dest, char *src)
+{
+	int i, len = 0;
+	char *cutpoint = &dest[n];
+
+	for (i = 0; dest[i] != '\0'; i++)
+	{
+		if (i < n)
+		{
+			dest[i] = src[i];
+			len++;
+		}
+	}
+	return (dest);
+}
