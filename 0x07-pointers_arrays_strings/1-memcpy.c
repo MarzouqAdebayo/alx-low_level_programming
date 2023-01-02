787c239ccf30c2559b2bccc4bcca747c16815916
marzouq <marzouqaadebayo@gmail.com>
2023-01-02 16:54:16 +0000

    Task 2 updated

--- 0x07-pointers_arrays_strings/1-memcpy.c
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -1,17 +1,17 @@
 #include "main.h"
 
 /**
- * *_memcpy - Funtion to replace array values up to n bytes with b
- * @dest: pointer to the array
- * @src: character to replace the value with
+ * *_memcpy - copies the memory area
+ * @dest: pointer to the destination array
+ * @src: pointer to the source array
  * @n: number of byte up to which to replace values
  *
- * Return: pointer to the array
+ * Return: pointer to the destination array
  */
 
 char *_memcpy(char *dest, char *src, unsigned int n)
 {
-	int i;
+	unsigned int i;
 
 	for (i = 0; i < n; i++)
 		dest[i] = src[i];
