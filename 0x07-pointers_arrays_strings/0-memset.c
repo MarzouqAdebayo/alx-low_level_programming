3c7084cb5500bb04e1f0bbeec76cdcf65c693649
marzouq <marzouqaadebayo@gmail.com>
2023-01-02 16:31:03 +0000

    Added include directives to all files

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -1,8 +1,8 @@
 #include "main.h"
 
 /**
- * *_memset - Funtion to replace array values up to n bytes with b
- * @s: pointer to the array
+ * _memset - fills memory with a constant byte
+ * @s: pointer to the array in memory
  * @b: character to replace the value with
  * @n: number of byte up to which to replace values
  *
@@ -11,7 +11,7 @@
 
 char *_memset(char *s, char b, unsigned int n)
 {
-	int i;
+	unsigned int i;
 
 	for (i = 0; i < n; i++)
 		s[i] = b;
