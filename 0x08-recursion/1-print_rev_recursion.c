30f66ff7150b58955e35cda0b8edccd53f14a9bb
marzouq <marzouqaadebayo@gmail.com>
2023-01-04 09:34:35 +0000

    Task 4 done

---
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * _print_rev_recursion - prints a string to stdout
+ * @s: string to be printed
+ *
+ * Return: void, has no return value
+ */
+
+void _print_rev_recursion(char *s)
+{
+	if (*s == '\0')
+	{
+		_putchar('\n');
+		return;
+	}
+	_puts_recursion((s + 1));
+	_putchar(*s);
+}
---
+++ 0x08-recursion/3-factorial.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * factorial - returns the factorial of a number
+ * @n: number whose factorial is to be returned
+ *
+ * Return: int, returns the factorial
+ */
+
+void factorial(char *s)
+{
+	if (n < 0)
+		return (-1);
+	if (n == 0)
+		return (1);
+	return (n * factorial(n - 1));
+}
