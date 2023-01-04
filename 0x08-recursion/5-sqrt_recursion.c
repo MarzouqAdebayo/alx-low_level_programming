b5c00088a4acc84acc0d321698561265b0c523ea
marzouq <marzouqaadebayo@gmail.com>
2023-01-04 11:01:14 +0000

    Completed task 6

---
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -0,0 +1,32 @@
+#include "main.h"
+
+/**
+ * multiply - multiplies a number by itself and checks it
+ * @i: number
+ * @num: The number whose sqrt is to be found
+ *
+ * Return: int, sqaure root or -1
+ */
+
+int multiply(int i, int num)
+{
+	if (i * i == num)
+		return (i);
+	if (i * i > num)
+		return (-1);
+	return (multiply(i + 1, num));
+}
+
+/**
+ * _sqrt_recursion(int n)
+ * @n: number whose sqrt is to be found
+ *
+ * Return: sqrt of n or 0
+ */
+
+int _sqrt_recursion(int n)
+{
+	if (n == 0)
+		return (0);
+	return (multiply(1, n));
+}
---
+++ 0x08-recursion/6-is_prime_number.c
@@ -0,0 +1,32 @@
+#include "main.h"
+
+/**
+ * divide - divides a number by an int and checks it
+ * @i: number
+ * @num: The number to be checked
+ *
+ * Return: int, 0 or 1
+ */
+
+int divide(int i, int num)
+{
+	if (i == 1)
+		return (1);
+	if (num % i == 0)
+		return (0);
+	return (divide(i - 1, num));
+}
+
+/**
+ * is_prime_number(int n)
+ * @n: number to be checked if prime or not
+ *
+ * Return: int, 1 or 0
+ */
+
+int is_prime_number(int n)
+{
+	if (n <= 0)
+		return (0);
+	return (divide((n + 1) / 2, n));
+}
