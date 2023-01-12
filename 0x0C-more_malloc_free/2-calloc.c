f954361f187cad27af52b98245dfb900efeae098
marzouq <marzouqaadebayo@gmail.com>
2023-01-12 23:54:02 +0000

    bug fixes:

--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -11,13 +11,13 @@
 
 void *_calloc(unsigned int nmemb, unsigned int size)
 {
-	char *p;
+	char *ptr;
 	unsigned int i;
 
 	if (nmemb == 0 || size == 0)
 		return (NULL);
-	p = malloca(nmemb * size);
-	if (p == NULL)
+	ptr = malloc(nmemb * size);
+	if (ptr == NULL)
 		return (NULL);
 	for (i = 0; i < nmemb; i++)
 		ptr[i] = 0;
