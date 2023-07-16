3082f040efffcbf76a8e7f212617238847c8cf9c
Marzouq Adebayo <marzouqaadebayo@gmail.com>
2023-07-16 20:03:25 +0000

    Task 2 done

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/**
+ * main - Entry Point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int a = 97, z = 0;
+
+	for (;a < 97+26; a++)
+		putchar(a);
+	putchar('\n');
+}
