590c1309e5c000775f8a04abc63f7e5e2fbdb786
Marzouq Adebayo <marzouqaadebayo@gmail.com>
2023-07-18 19:41:12 +0000

    Task 100 done

---
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -0,0 +1,52 @@
+#include "main.h"
+/**
+ * times_table - Contains full body of the function
+ *
+ * Description: Print the times table from 0-9
+ * Return: void, has no return value
+ */
+void print_times_table(int n)
+{
+	int i, j, multiplication, remainder;
+
+	if (n > 15 || n < 0)
+		return;
+	for (i = 0; i < n + 1; i++)
+	{
+		for (j = 0; j < n + 1; j++)
+		{
+			multiplication = i * j;
+
+			if (j != 0)
+			{
+				if (multiplication < 10)
+				{
+					_putchar(' ');
+					_putchar(' ');
+					_putchar(' ');
+					_putchar(48 + (multiplication));
+				}
+				else if (multiplication < 100)
+				{
+					_putchar(' ');
+					_putchar(' ');
+					_putchar(48 + (multiplication / 10));
+					_putchar(48 + multiplication % 10);
+				}
+				else
+				{
+					_putchar(' ');
+					_putchar(48 + (multiplication / 100));
+					remainder = multiplication % 100;
+					_putchar(48 + (remainder / 10));
+					_putchar(48 + multiplication % 10);
+				}
+			}
+			else
+				_putchar(48);
+			if (j != n)
+				_putchar(',');
+		}
+		_putchar('\n');
+	}
+}
