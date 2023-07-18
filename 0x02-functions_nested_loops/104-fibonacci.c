7e07a478c647f5144e8d9aea9f40c8f5e0d14e21
Marzouq Adebayo <marzouqaadebayo@gmail.com>
2023-07-18 21:18:03 +0000

    Task 104

---
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -0,0 +1,26 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always (0) success
+ */
+
+int main(void)
+{
+	int n = 0;
+	unsigned long i = 0, j = 1, k;
+
+	while (n < 98)
+	{
+		printf("%lu", (i + j));
+		if (n < 97)
+			printf(", ");
+		k = i + j;
+		i = j;
+		j = k;
+		n++;
+	}
+	return (0);
+}
