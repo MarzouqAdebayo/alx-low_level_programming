400c9df159c90b3f6f69fa39ec38e878fdd07e16
Marzouq Adebayo <marzouqaadebayo@gmail.com>
2023-07-18 04:50:05 +0000

    task 10 done

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+/**
+ * main - Entry Point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int a = 0, b = 0;
+
+	while (a < 10)
+	{
+		while (b < 10)
+		{
+			if (b ==a || b < a)
+			{
+				b++;
+				continue;
+			}
+			putchar(a + 48);
+			putchar(b + 48);
+			if (a != 8 || b != 9)
+			{
+				putchar(',');
+				putchar(' ');
+			}
+			b++;
+		}
+		a++;
+		b = 0;
+	}
+	return (0);
+}
