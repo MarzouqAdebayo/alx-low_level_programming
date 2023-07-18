b7ef56cbac0e83f0b9e5b9b1b2c5f703ca7188f0
Marzouq Adebayo <marzouqaadebayo@gmail.com>
2023-07-18 19:51:37 +0000

    Task 101 done

---
+++ 0x02-functions_nested_loops/101-natural.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always (0) success
+*/
+
+int main(void)
+{
+	int n = 0, sum = 0;
+	while (n < 1024)
+	{
+		if (n % 3 == 0 || n % 5 == 0)
+			sum += n;
+		n++;
+	}
+	printf("%d", sum);
+	return (0);
+}
