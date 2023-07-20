a72635ffd5425a4496b0e800fb4ee5c8b31704aa
Marzouq Adebayo <marzouqaadebayo@gmail.com>
2023-07-20 20:57:42 +0000

    Tasks done

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -1,1 +1,15 @@
+#include "main.h"
+/**
+ * _isupper - Body of the function
+ * @c: character decimal value to be checked
+ *
+ * Description: Checks if a character is uppercase of not
+ * Return: 1 if c is uppercase otherwise 0
+ */
+int _isupper(int c)
+{
+	if (c >= 65 && c < 65 + 26)
+		return (1);
+	return (0);
+}
 
