5ad9d2790bb64b2fb4f9936e2360da68bb67ba41
Marzouq Adebayo <marzouqaadebayo@gmail.com>
2023-07-24 21:50:37 +0000

    task 4

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ * _puts - Body of the function
+ * @str: pointer to string
+ *
+ * Description: Prints a string to stdout
+ * Return: has no return value
+ */
+
+void _puts(char *str)
+{
+	int i = 0;
+
+	while (str[i] != '\0')
+	{
+		putchar(str[i]);
+		i++;
+	}
+	_putchar('\n');
+}
---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,23 @@
+#include "main.h"
+/**
+ * print_rev - Body of the function
+ * @s: pointer to string
+ *
+ * Description: Prints a string to stdout in reverse
+ * Return: has no return value
+ */
+
+void print_rev(char *s)
+{
+	int len = 0;
+
+	for (len = 0; s[len] != '\0'; len++)
+		;
+
+	while (len >= 0)
+	{
+		_putchar(s[len]);
+		len--;
+	}
+	_putchar('\n');
+}
