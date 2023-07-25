15bcde6ed361dd4f521b150ce83ec65cefa9a73b
Marzouq Adebayo <marzouqaadebayo@gmail.com>
2023-07-25 19:33:29 +0000

    Task 100 done

---
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -0,0 +1,30 @@
+#include "main.h"
+/**
+ * _atoi - Body of the function
+ * @s: The character to be parsed
+ *
+ * Description: converts strings into int
+ * Return: has no return value
+ */
+
+int _atoi(char *s)
+{
+	int i, result = 0, sign = 1;
+
+	while (*(s + i) != '\0')
+	{
+		if (s[i] == '-')
+		{
+			sign *= -1;
+			i++;
+			continue;
+		}
+		if (*(s + i) >= '0' && *(s + i) <= '9')
+		{
+			result = (result * 10) + (s[i] - 48);
+		}
+		i++;
+	}
+
+	return (result * sign);
+}
