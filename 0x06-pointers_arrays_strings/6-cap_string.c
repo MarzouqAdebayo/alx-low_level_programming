20997a6bfaa3142fdeb08a4347d53d8cd6306220
Marzouq Adebayo <marzouqaadebayo@gmail.com>
2023-07-27 18:52:38 +0000

    task 6 done

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -4,19 +4,18 @@
  * string_toupper - changes string to uppercase
  * @str: Pointer to the string
  *
- * Return: pointer to the string
+ * Return: Pointer to the string
 */
 
 char *string_toupper(char *str)
 {
 	int i;
-	char arr[26] = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
 
 	for (i = 0; str[i]; i++)
 	{
-		if (str[i] >= 97 && str[i] <= (97 + 26))
+		if (str[i] >= 'a' && str[i] <= 'A')
 		{
-			str[i] = arr[str[i] % 97];
+			str[i] -= 'a' - 'A';
 		}
 	}
 
