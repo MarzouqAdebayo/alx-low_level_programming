1d4be3d3e2c159ee1f41ad988c14f451917aefa7
Marzouq Adebayo <marzouqaadebayo@gmail.com>
2023-08-02 13:58:44 +0000

    Fixed betty errors

--- 0x08-recursion/6-is_prime_number.c
+++ 0x08-recursion/6-is_prime_number.c
@@ -14,7 +14,7 @@ int check_prime(int n, int i)
 		return (1);
 	if ((n % i) == 0)
 		return (0);
-	return check_prime(n, (i - 1));
+	return (check_prime(n, (i - 1)));
 }
 
 /**
@@ -28,5 +28,5 @@ int is_prime_number(int n)
 {
 	if (n <= 1)
 		return (0);
-	return check_prime(n, n / 2);
+	return (check_prime(n, n / 2));
 }
