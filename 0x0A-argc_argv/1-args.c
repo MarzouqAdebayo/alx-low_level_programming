7bb16bbaa9b92942c69c89f3b83f63216fc72068
Marzouq Adebayo <marzouqaadebayo@gmail.com>
2023-08-04 13:51:25 +0000

    Task 1

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ * main - prints the number of arguments passed
+ * @argc: number of arguments
+ * @argv: pointer to the arguments
+ *
+ * Return: Always 0
+ */
+
+int main(int argc, char *argv[] __attribute__((unused)))
+{
+	_putchar(0);
+	return (0);
+}
+
