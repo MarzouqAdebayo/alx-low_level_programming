4ed3d7fbb743f4d375928bcfc67fbb8a4859e330
Marzouq Adebayo <marzouqaadebayo@gmail.com>
2023-08-07 12:54:30 +0000

    Task 1 done

---
+++ 0x0B-malloc_free/1-strdup.c
@@ -0,0 +1,32 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ * *_strdup - Creates an array of characters
+ * @str: String to be copied
+ *
+ * B
+ * Return: Pointer to the new string
+ */
+
+char *_strdup(char *str)
+{
+	char *new;
+	int i;
+
+	for (i = 0; *(str + i); i++)
+		;
+
+	new = malloc(sizeof(*new) * i);
+
+	if (!new)
+		return (0);
+	i = 0;
+	while (*(str + i))
+	{
+		*(new + i) = *(str + i);
+		i++;
+	}
+	*(new + i) = '\0';
+	return (new);
+}
