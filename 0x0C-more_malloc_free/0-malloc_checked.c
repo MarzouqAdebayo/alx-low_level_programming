d4b1ab05626ac4e3d4fd8ca0b1dc296946cca587
Marzouq Adebayo <marzouqaadebayo@gmail.com>
2023-08-10 12:08:21 +0000

    Task 0 done

---
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * malloc_checked - Function allocates memory using malloc
+ * @b: size of memory to be allocated
+ *
+ * Return: pointer to the address allocated
+*/
+
+void *malloc_checked(unsigned int b)
+{
+	void *ptr;
+
+	ptr = malloc(b);
+	if (!ptr)
+		exit(98);
+	return (ptr);
+}
