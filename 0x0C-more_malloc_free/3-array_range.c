34a0e38a486744f9219943e28e545bf64c182358
Marzouq Adebayo <marzouqaadebayo@gmail.com>
2023-08-10 15:02:20 +0000

    Task 3 bug fix

--- 0x0C-more_malloc_free/3-array_range.c
+++ 0x0C-more_malloc_free/3-array_range.c
@@ -14,11 +14,10 @@ int *array_range(int min, int max)
 	int *ptr, i;
 
 	if (min > max)
-		return (NULL);
-
+		return (0);
 	ptr = malloc(sizeof(int *) * (max - min + 1));
 	if (!ptr)
-		return (NULL);
+		return (0);
 	for (i = min; i <= max; i++)
 		*(ptr + i - min) = i;
 	return (ptr);
