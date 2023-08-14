bf6c8e16604a71185cb6112c7bb314de89d5f89f
Marzouq Adebayo <marzouqaadebayo@gmail.com>
2023-08-14 17:12:34 +0000

    task 3

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -16,6 +16,9 @@ dog_t *new_dog(char *name, float age, char *owner)
 	dog_t *new;
 	char *new_name, *new_owner;
 
+	if (!name || !owner)
+		return (NULL);
+
 	new = malloc(sizeof(*new));
 	if (!new)
 		return (NULL);
