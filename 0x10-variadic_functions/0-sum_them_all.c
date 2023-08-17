b2f508e76a1d4e60f0e2f1c729984a3e7b6e5a14
Marzouq Adebayo <marzouqaadebayo@gmail.com>
2023-08-17 18:10:16 +0000

    Task 0

---
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -0,0 +1,23 @@
+#include "variadic_functions.h"
+
+/**
+ * sum_them_all - Sums an infinite number of integers
+ * @n: number of arguments passed
+ *
+ * Return: sum of the integers
+*/
+
+int sum_them_all(const unsigned int n, ...)
+{
+	int sum = 0;
+	unsigned int i;
+	va_list args;
+	va_start(args, n);
+
+	if (n <= 0)
+		return (0);
+	for (i = 0; i < n; i++)
+		sum += va_arg(args, int);
+	va_end(args);
+	return (sum);
+}
