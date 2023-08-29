9ede0cfaed7cb4ed86d398cf139c63ec65a9f99b
Marzouq Adebayo <marzouqaadebayo@gmail.com>
2023-08-29 12:58:06 +0000

    Task 10 bug fix 5

--- 0x13-more_singly_linked_lists/10-delete_nodeint.c
+++ 0x13-more_singly_linked_lists/10-delete_nodeint.c
@@ -10,36 +10,27 @@
 
 int delete_nodeint_at_index(listint_t **head, unsigned int index)
 {
-	listint_t *current, *prev;
+	listint_t *current = *head, *prev = *head;
 	unsigned int i = 0;
 
-	if (!head)
-		return (-1);
-
-	current = *head;
-	prev = *head;
-	if (index == 0)
+	i = 0;
+	while (current)
 	{
-		*head = prev->next;
-		free(prev);
-		return (1);
-	}
-	else
-	{
-		while (current->next)
+		if (index == 0)
 		{
-			if (i == index)
-			{
-				prev->next = current->next;
-				free(current);
-				return (1);
-			}
-			prev = current;
-			current = current->next;
-			i++;
-			if (!current)
-				break;
+			*head = prev->next;
+			free(prev);
+			return (1);
 		}
+		else if (i == index)
+		{
+			prev->next = current->next;
+			free(current);
+			return (1);
+		}
+		prev = current;
+		current = current->next;
+		i++;
 	}
 	return (-1);
 }
