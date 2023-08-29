8c01bad198071709d1701c4dba8b8eec22a4d317
Marzouq Adebayo <marzouqaadebayo@gmail.com>
2023-08-29 11:24:04 +0000

    Task 9 bug fix 2

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -33,6 +33,7 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 				new->n = n;
 				new->next = current->next;
 				current->next = new;
+				return (new);
 			}
 			current = current->next;
 			i++;
