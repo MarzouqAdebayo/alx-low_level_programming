37f632f3c69d026e9a227ce9429612d27048dd5f
Marzouq Adebayo <marzouqaadebayo@gmail.com>
2023-09-01 15:37:30 +0000

    Task 1 done

---
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -0,0 +1,33 @@
+#include "main.h"
+
+/**
+ * print_binary - Print the binary representation of an integer
+ * @n: number to be printed
+ *
+ * Return: has no return value
+*/
+
+void print_binary(unsigned long int n)
+{
+	int num_bits, i;
+
+	if (n == 0)
+	{
+		printf("0");
+		return;
+	}
+
+	num_bits = sizeof(n) * 8;
+
+	for (i = num_bits - 1; i >= 0; i--)
+	{
+		unsigned long int mask = 1UL << i;
+		if (n & mask) {
+			printf("1");
+		}
+		else
+		{
+			printf("0");
+		}
+	}
+}
