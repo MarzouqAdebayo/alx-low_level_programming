3ba012a815f5d86e3655189398ad92a247547ab8
Marzouq Adebayo <marzouqaadebayo@gmail.com>
2023-09-01 17:07:37 +0000

    Task 2 bug fix 1

--- 0x14-bit_manipulation/2-get_bit.c
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -6,10 +6,15 @@
  * @index: index of bit ot be checked
  *
  * Return: 1 | 0 | -1
-*/
+ */
 
 int get_bit(unsigned long int n, unsigned int index)
 {
+	unsigned long int num_bits;
+
+	num_bits = sizeof(unsigned int) * 8;
+	if (index < 0 || index >= num_bits)
+		return (-1);
 	unsigned long int mask = 1 << index;
 
 	return (n & mask ? 1 : 0);
