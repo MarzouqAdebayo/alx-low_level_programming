d5abf52b9a3bd3671cc1db91aa25e77c50c7434b
Marzouq Adebayo <marzouqaadebayo@gmail.com>
2023-09-01 16:45:56 +0000

    Task 2 bug fix 2

--- 0x14-bit_manipulation/1-print_binary.c
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -5,25 +5,32 @@
  * @n: number to be printed
  *
  * Return: has no return value
-*/
+ */
 
 void print_binary(unsigned long int n)
 {
 	int num_bits, i;
 	unsigned long int mask;
+	int flag = 0;
 
 	if (n == 0)
 	{
 		printf("0");
 		return;
 	}
+
 	num_bits = sizeof(n) * 8;
 	for (i = num_bits - 1; i >= 0; i--)
 	{
-		mask = 1UL << i;
-		if (n & mask)
+		mask = 1 << i;
+		if (n & mask) {
+			flag = 1;
 			printf("1");
+		}
 		else
-			printf("0");
+		{
+			if (flag)
+				printf("0");
+		}
 	}
 }
