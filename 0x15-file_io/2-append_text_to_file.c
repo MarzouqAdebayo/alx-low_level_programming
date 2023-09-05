d462a4c63b3d6db5ff1da4af2f611b5e1d081c89
Marzouq Adebayo <marzouqaadebayo@gmail.com>
2023-09-05 16:46:28 +0000

    Task 2 done

---
+++ 0x15-file_io/2-append_text_to_file.c
@@ -0,0 +1,34 @@
+#include "main.h"
+
+/**
+ * append_text_to_file - appends text to file
+ * @filename: name of the file
+ * @text_content: text to append to file
+ *
+ * Return: 1 if successful else -1
+ */
+
+int append_text_to_file(const char *filename, char *text_content)
+{
+	if (!filename)
+		return (-1);
+
+	int file = open(filename, O_WRONLY | O_APPEND);
+
+	if (file == -1)
+		return (-1);
+
+	if (text_content)
+	{
+		ssize_t bytes_written = write(file, text_content, strlen(text_content));
+
+		if (bytes_written == -1)
+		{
+			close(file);
+			return (-1);
+		}
+	}
+
+	close(file);
+	return (1);
+}
