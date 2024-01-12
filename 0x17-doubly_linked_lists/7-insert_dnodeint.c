e53b4e0e2636eae60d8a4bd44d654dff37f702d9
Marzouq Adebayo <marzouqaadebayo@gmail.com>
2024-01-12 10:29:41 +0000

    Tasks 7 bug fix 4

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -15,6 +15,8 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 
 	if (!h)
 		return (NULL);
+	else if (!(*h)->next && idx != 0)
+		return (NULL);
 	else if (idx == 0)
 		return (add_dnodeint(h, n));
 	new_node = malloc(sizeof(dlistint_t));
