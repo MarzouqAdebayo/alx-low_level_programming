83989ece0db54f80bb633e3369c37a221c4b301e
Marzouq Adebayo <marzouqaadebayo@gmail.com>
2024-03-28 02:06:56 +0000

    completed task 5

--- 0x1C-makefiles/5-island_perimeter.py
+++ 0x1C-makefiles/5-island_perimeter.py
@@ -28,22 +28,23 @@ def island_perimeter(grid):
     perimeter = 0
 
     for i in range(len(grid)):
-        for j in range(len(grid)):
+        for j in range(len(grid[0])):
             if (grid[i][j] == 1):
-                try:
-                    perimeter += 1 if grid[i - 1][j] == 0 else 0
-                except IndexError:
-                    perimeter += 1
-                try:
-                    perimeter += 1 if grid[i + 1][j] == 0 else 0
-                except IndexError:
-                    perimeter += 1
-                try:
-                    perimeter += 1 if grid[i][j - 1] == 0 else 0
-                except IndexError:
-                    perimeter += 1
-                try:
-                    perimeter += 1 if grid[i][j + 1] == 0 else 0
-                except IndexError:
-                    perimeter += 1
+                if i - 1 >= 0:
+                    top = 1 if grid[i - 1][j] == 0 else 0
+                else:
+                    top = 1
+                if i + 1 < len(grid):
+                    bottom = 1 if grid[i + 1][j] == 0 else 0
+                else:
+                    bottom = 1
+                if j - 1 >= 0:
+                    left = 1 if grid[i][j - 1] == 0 else 0
+                else:
+                    left = 1
+                if j + 1 < len(grid[0]):
+                    right = 1 if grid[i][j + 1] == 0 else 0
+                else:
+                    right = 1
+                perimeter += top + bottom + left + right
     return perimeter
